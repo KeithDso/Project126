982df25804e6c4f29f6fb6c015b884d726f7759c
KeithDso <132676143+KeithDso@users.noreply.github.com>
2024-06-11 18:52:28 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,18 +3,46 @@ function setup(){
     canvas.center();
 
     video = createCapture(VIDEO);
-    video.hide()
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoad(){
+    console.log('PoseNet is initialized');
 }
 
 function draw(){
-    Image(video, 0, 0, 600, 500)
+    Image(video, 0, 0, 600, 500);
 }
 
-song1="music.mp3";
-song2="music2.mp3";
+song="";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload(){
-    song = loadSound("music.mp3")
-    song2 = loadSound("music2.mp3")
+    song = loadSound("music.mp3");
+}
+
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses(){
+    if(results.length > 0 ){
+        console.log(results)
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +"leftWristY = "+leftWristY);
+
+        righttWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +"rightWristY = "+rightWristY);
+    }
 }
 
